9413e772a043ef34dd6d1895d6ae9f13281c9720
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2024-01-12 10:10:21 +0700

    fix: fixing logic middleware

--- App/Middleware/PdfMiddleware.js
+++ App/Middleware/PdfMiddleware.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const ErrorHandling = require('../Error/ErrorHandling');
 const ResponseError = require('../Error/ResponseError');
 
@@ -5,7 +6,10 @@ module.exports = {
   minFileUpload(req, res, next) {
     try {
       const { files } = req;
-      if (files.length < 2) ErrorHandling.badRequest('Minimal 2 files yang diupload');
+      if (files.length < 2) {
+        fs.unlinkSync(files[0].path);
+        ErrorHandling.badRequest('Minimal 2 files yang diupload');
+      }
 
       next();
     } catch (error) {
--- App/Routes/PdfRoutes.js
+++ App/Routes/PdfRoutes.js
@@ -6,7 +6,7 @@ const PdfMiddleware = require('../Middleware/PdfMiddleware');
 const route = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
-route.post('/uploads/pdf', PdfMiddleware.minFileUpload, upload.array('files', 5), PdfController.upload);
+route.post('/uploads/pdf', upload.array('files', 5), PdfMiddleware.minFileUpload, PdfController.upload);
 route.get('/download/pdf/:path', PdfController.download);
 
 module.exports = route;
